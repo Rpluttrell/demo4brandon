ee848e641828f6d1df2451a580203a10ab54a0e8
Reed Luttrell <reed@Michaels-iMac.local>
2018-06-03 17:51:05 -0700

    first commit of demo project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/www'))
+
+app.listen(port)
+
+console.log(`server listening on port ${port}`)
+
